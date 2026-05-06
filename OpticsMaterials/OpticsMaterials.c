80becb65d29b620576c2aaba8ef08cbbaa31575b
oguyon <oliv.guyon@gmail.com>
2026-05-06 01:18:14 -1000

    feat: restructure OpticsMaterials as a standalone plugin

--- OpticsMaterials/OpticsMaterials.c
+++ OpticsMaterials/OpticsMaterials.c
@@ -27,7 +27,7 @@
 /* ================================================================== */
 /* ================================================================== */
 
-#include "CommandLineInterface/CLIcore.h"
+#include "CLIcore.h"
 
 //
 // Forward declarations are required to connect CLI calls to functions
